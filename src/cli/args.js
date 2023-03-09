ccf306797a2151f61436384d7b488ce4c6599f85
Denis <apricot103@mail.ru>
2023-03-10 00:09:11 +0300

    added args lesson realization

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    let firstArg = process.argv[2];
+    let secondArg = process.argv[3];
+
+    console.log("PropName1 is " + firstArg);
+    console.log("PropName2 is " + secondArg);
 };
 
 parseArgs();
